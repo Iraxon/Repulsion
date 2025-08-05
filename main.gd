5114691691377e7aa0252240c085c2df1e87888c
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-08-05 16:46:50 -0400

    Initial commit

---
+++ main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const MENU = preload("res://start_menu.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_child(MENU.instantiate())
+
+func handle_menu_choice(i: int):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
